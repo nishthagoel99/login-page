079c26f54d3167628df48614b74f305655c57d14
Nishtha Goel <31266528+nishthagoel99@users.noreply.github.com>
2018-06-18 14:17:56 +0530

    login-page_project
    A webpage where you can signup and then sign-in.

---
+++ app.js
@@ -0,0 +1,77 @@
+var express=require('express');
+var path=require('path');
+var app=express();
+var mysql=require('mysql');
+var bodyParser=require('body-parser');
+
+var con=mysql.createConnection({
+    host:"localhost",
+    password:"",
+    user:"root",
+    database:"login"
+});
+//////////////////////////////
+
+//////connect mysql
+con.connect(function(err){
+if(err){
+    console.log('error!');
+}else
+console.log('Connected');
+});
+////////////////////
+
+app.use(express.static(path.join(__dirname , '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res) => {
+    res.sendfile(__dirname + '/home.html');
+});
+
+app.post('/details1',function(req,res){
+
+   var Email=req.body.mail1;
+   var  Password=req.body.password1;
+
+con.query('SELECT * FROM mytable WHERE Email=? ',[Email],function(error,rows,fields){
+    if(error){
+        console.log('error in matching');
+    }
+    else {
+       if(rows.length>0){ 
+            if(Password==rows[0].Password){
+            console.log('success');
+                res.send('Login Successful!');
+            }else
+             {
+            console.log('email and password dont match');
+            res.send('Wrong Password!');
+             }
+        }   
+      else{
+        console.log("email is not present");
+        res.send('You haven\'t registerd! Sign up first!');    
+         }
+        }   
+    });
+});
+app.post('/details',function(req,res){
+    var newuser={
+    Name:req.body.user,
+    Email:req.body.mail,
+    Password:req.body.password
+}
+con.query('INSERT INTO mytable SET?',newuser,function(error,resp){
+if(error){
+    console.log('error in query');
+    res.send('Enter proper values!');
+}else
+{
+    console.log('connected in query');
+    res.send('Welcome! You have successfully registered!');
+}
+});
+});
+
+app.listen('3307');
